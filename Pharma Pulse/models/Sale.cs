aa944a3ceb0bd9ff4bcafead9674b832659eaa4c
Ram Acharya <2303031087013@paruluniversity.ac.in>
2026-02-09 15:02:31 +0530

    created salessummery page, quickbilling page

---
+++ Pharma Pulse/models/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pharma_Pulse.Models
+{
+    public class Sale
+    {
+        public string MedicineName { get; set; }
+
+        public int QuantitySold { get; set; }
+
+        public DateTime SaleDate { get; set; }
+
+        public decimal TotalAmount { get; set; }
+
+        public decimal Profit { get; set; }
+    }
+}
---
+++ Pharma Pulse/models/SalesService.cs
@@ -0,0 +1,23 @@
+﻿using Pharma_Pulse.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pharma_Pulse.Services
+{
+    public static class SalesService
+    {
+        private static List<Sale> SalesList = new List<Sale>();
+
+        // Add Sale Record (Billing ke baad call होगा)
+        public static void AddSale(Sale sale)
+        {
+            SalesList.Add(sale);
+        }
+
+        // Get All Sales
+        public static List<Sale> GetAllSales()
+        {
+            return SalesList;
+        }
+    }
+}
