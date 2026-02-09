e69def5922e45c5975532c2ac37e23d828858669
Ram Acharya <2303031087013@paruluniversity.ac.in>
2026-02-09 17:31:28 +0530

    added medicine form

---
+++ Pharma Pulse/Pages/AddMedicine.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pharma_Pulse.Models;
+using Pharma_Pulse.Services;
+
+namespace Pharma_Pulse.Pages
+{
+    public class AddMedicineModel : PageModel
+    {
+        [BindProperty]
+        public Medicine Medicine { get; set; }
+
+        public string SuccessMessage { get; set; }
+
+        public void OnGet()
+        {
+            Medicine = new Medicine();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            // ✅ Add medicine into MedicineService list (temporary)
+            MedicineService.AddMedicine(Medicine);
+
+            SuccessMessage = "Medicine Added Successfully!";
+
+            return Page();
+        }
+    }
+}
