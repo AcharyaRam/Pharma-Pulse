2dcb44033c61774835a35ccad8008611b2fd4399
Ram Acharya <2303031087013@paruluniversity.ac.in>
2026-02-13 18:42:18 +0530

    addmedicine fields added

--- Pharma Pulse/Migrations/20260213122901_AddBatchMfgHsnFields.Designer.cs
+++ Pharma Pulse/Migrations/20260213122901_AddBatchMfgHsnFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pharma_Pulse.Data;
 
@@ -11,9 +12,11 @@ using Pharma_Pulse.Data;
 namespace Pharma_Pulse.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260213122901_AddBatchMfgHsnFields")]
+    partial class AddBatchMfgHsnFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Pharma Pulse/Migrations/20260213122901_AddBatchMfgHsnFields.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pharma_Pulse.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBatchMfgHsnFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BatchNo",
+                table: "Medicines",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HsnSac",
+                table: "Medicines",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "MfgDate",
+                table: "Medicines",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BatchNo",
+                table: "Medicines");
+
+            migrationBuilder.DropColumn(
+                name: "HsnSac",
+                table: "Medicines");
+
+            migrationBuilder.DropColumn(
+                name: "MfgDate",
+                table: "Medicines");
+        }
+    }
+}
