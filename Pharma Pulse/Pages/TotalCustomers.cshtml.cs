c2b42d5bf5a7298f74ada20fc1225d8954c3713e
Ram Acharya <2303031087013@paruluniversity.ac.in>
2026-02-14 15:53:14 +0530

    New Changes

---
+++ Pharma Pulse/Pages/TotalCustomers.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pharma_Pulse.Data;
+using Pharma_Pulse.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Pharma_Pulse.Pages
+{
+    public class TotalCustomersModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public TotalCustomersModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Customer> Customers { get; set; } = new();
+
+        public string SearchTerm { get; set; } = "";
+
+        public void OnGet(string search)
+        {
+            SearchTerm = search;
+
+            Customers = _context.Customers
+                .Where(c =>
+                    string.IsNullOrEmpty(search) ||
+                    c.CustomerName.Contains(search) ||
+                    c.MobileNumber.Contains(search)
+                )
+                .ToList();
+        }
+    }
+}
