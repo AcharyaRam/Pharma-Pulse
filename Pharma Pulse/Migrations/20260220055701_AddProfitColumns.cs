eb97c0137f4b570b5046d8a029c3cc4629612865
Meet Borade <meetborade01@gmail.com>
2026-02-27 11:39:14 +0530

    All Over Changes

---
+++ Pharma Pulse/Migrations/20260220055701_AddProfitColumns.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pharma_Pulse.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfitColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PurchasePrice",
+                table: "BillDetails",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "SellingPrice",
+                table: "BillDetails",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PurchasePrice",
+                table: "BillDetails");
+
+            migrationBuilder.DropColumn(
+                name: "SellingPrice",
+                table: "BillDetails");
+        }
+    }
+}
