fb56ae214d49207daacff4ba6ec95c3032390730
Meet Borade <meetborade01@gmail.com>
2026-03-23 16:11:30 +0530

    azure deployed

---
+++ Pharma Pulse/Migrations/20260323103845_AddPricesToBillDetails.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pharma_Pulse.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPricesToBillDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
