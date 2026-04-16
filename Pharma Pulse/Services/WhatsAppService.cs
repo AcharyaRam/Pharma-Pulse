4bc3bb12f3195dc8eb9ab80f16299ad09c3f6daf
Ram Acharya <ramacharya333@gmail.com>
2026-04-16 17:25:25 +0530

    whatsapp integration

---
+++ Pharma Pulse/Services/WhatsAppService.cs
@@ -0,0 +1,20 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+public class WhatsAppService
+{
+    public WhatsAppService()
+    {
+        TwilioClient.Init("YOUR_ACCOUNT_SID", "YOUR_AUTH_TOKEN");
+    }
+
+    public void SendWhatsApp(string to, string message)
+    {
+        MessageResource.Create(
+            from: new PhoneNumber("whatsapp:+14155238886"), // Twilio sandbox
+            to: new PhoneNumber("whatsapp:" + to),
+            body: message
+        );
+    }
+}
